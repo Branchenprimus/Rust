abe2a89e2c8b1c80fc6cee4fba4161309f8a6548
Jan Wardenga <Jan.Wardenga@ibm.com>
2024-02-10 16:39:33 +0100

    first

---
+++ notiontocalendar/target/debug/deps/notiontocalendar-319a80962bc645a8.d
@@ -0,0 +1,5 @@
+/Users/janwardenga/Documents/Privat/Rust/notiontocalendar/target/debug/deps/libnotiontocalendar-319a80962bc645a8.rmeta: src/main.rs
+
+/Users/janwardenga/Documents/Privat/Rust/notiontocalendar/target/debug/deps/notiontocalendar-319a80962bc645a8.d: src/main.rs
+
+src/main.rs:
---
+++ notiontocalendar/target/debug/deps/notiontocalendar-c2ed6baa4e6f3685.d
@@ -0,0 +1,5 @@
+/Users/janwardenga/Documents/Privat/Rust/notiontocalendar/target/debug/deps/libnotiontocalendar-c2ed6baa4e6f3685.rmeta: src/main.rs
+
+/Users/janwardenga/Documents/Privat/Rust/notiontocalendar/target/debug/deps/notiontocalendar-c2ed6baa4e6f3685.d: src/main.rs
+
+src/main.rs:
